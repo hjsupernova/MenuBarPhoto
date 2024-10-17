3426ba8854cdfd167981a530fc67d7d81418ba4f
hjsupernova <kem08120506@gmail.com>
2024-10-17 22:22:41 +0900

    feat: Popover 배경색 변경 코드 작성

--- MenuBarPhoto/App/AppDelegate.swift
+++ MenuBarPhoto/App/AppDelegate.swift
@@ -31,10 +31,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.popover = NSPopover()
         self.popover.contentSize = NSSize(width: 300, height: 300)
         self.popover.behavior = .transient
-
-        let savedPhotos = CoreDataStack.shared.fetchPhotos()
-        self.popover.contentViewController = NSHostingController(rootView: HomeView(photos: savedPhotos))
         self.popover.animates = false
+
+        let hostingController = NSHostingController(rootView: HomeView(photos: CoreDataStack.shared.fetchPhotos()))
+        popover.backgroundColor = .white
+        self.popover.contentViewController = hostingController
 
         KeyboardShortcuts.onKeyUp(for: .togglePopover) {
             self.togglePopover()
@@ -114,5 +115,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func terminate() {
         NSApplication.shared.terminate(self)
+    }
+}
+
+// NSPopover background color
+extension NSPopover {
+    private struct Keys {
+        static var backgroundViewKey = "backgroundKey"
+    }
+
+    private var backgroundView: NSView {
+        let bgView = objc_getAssociatedObject(self, &Keys.backgroundViewKey) as? NSView
+        if let view = bgView {
+            return view
+        }
+
+        let view = NSView()
+        objc_setAssociatedObject(self, &Keys.backgroundViewKey, view, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        NotificationCenter.default.addObserver(self, selector: #selector(popoverWillOpen(_:)), name: NSPopover.willShowNotification, object: nil)
+        return view
+    }
+
+    @objc private func popoverWillOpen(_ notification: Notification) {
+        if backgroundView.superview == nil {
+            if let contentView = contentViewController?.view, let frameView = contentView.superview {
+                frameView.wantsLayer = true
+                backgroundView.frame = NSInsetRect(frameView.frame, 1, 1)
+                backgroundView.autoresizingMask = [.width, .height]
+                frameView.addSubview(backgroundView, positioned: .below, relativeTo: contentView)
+            }
+        }
+    }
+
+    var backgroundColor: NSColor? {
+        get {
+            if let bgColor = backgroundView.layer?.backgroundColor {
+                return NSColor(cgColor: bgColor)
+            }
+            return nil
+        }
+        set {
+            backgroundView.wantsLayer = true
+            backgroundView.layer?.backgroundColor = newValue?.cgColor
+        }
     }
 }
